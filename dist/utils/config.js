15b3eca60237f0ee9ebff79e20e362fa686b440c
Anuj Subedi <struggleage@gmail.com>
2026-01-07 21:31:54 +0545

    fix: final total calculation logic.
    fix: final total calculation logic when discount is not supplied.

--- dist/utils/config.js
+++ dist/utils/config.js
@@ -1,2 +1,7 @@
-'use strict';var e=(o,t)=>()=>(t||o((t={exports:{}}).exports,t),t.exports);var i=e((n,a)=>{var l={string:{invoice:"I N V O I C E",refNumber:"Ref no",date:"Date",dueDate:"Due Date",status:"Status",billTo:"Bill To",item:"Item",quantity:"Qty",price:"Price",discount:"Discount",tax:"Tax",total:"Total",subTotal:"Subtotal",totalTax:"Total Tax",totalDiscount:"Total Discount",fee:"Total Fee",grandTotal:"Grand Total"}};a.exports=l;});var config = i();module.exports=config;//# sourceMappingURL=config.js.map
+'use strict';
+
+var e=(o,t)=>()=>(t||o((t={exports:{}}).exports,t),t.exports);var i=e((n,a)=>{var l={string:{invoice:"I N V O I C E",refNumber:"Ref no",date:"Date",dueDate:"Due Date",status:"Status",billTo:"Bill To",item:"Item",quantity:"Qty",price:"Price",discount:"Discount",tax:"Tax",total:"Total",subTotal:"Subtotal",totalTax:"Total Tax",totalDiscount:"Total Discount",fee:"Total Fee",grandTotal:"Grand Total"}};a.exports=l;});var config = i();
+
+module.exports = config;
+//# sourceMappingURL=out.js.map
 //# sourceMappingURL=config.js.map
